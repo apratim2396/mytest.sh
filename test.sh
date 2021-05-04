da7f23c7bb31315bdc8d26fb34b736e6524496bd
Apratim Tripathi <57747247+apratim2396@users.noreply.github.com>
2021-05-04 13:03:34 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+echo "# mytest.sh" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/apratim2396/mytest.sh.git
+git push -u origin main
